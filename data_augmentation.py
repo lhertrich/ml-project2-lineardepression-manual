6ce022d89b8cd7ee3883eb6971eb9b0720b684ee
Eugenie Cyrot <eugenie.cyrot@epfl.ch>
2024-12-10 11:56:19 +0100

    Added in augmentation pipeline Gaussian Blur and color modifcations (contrast, brightness, saturation, RGB Shift)

--- data_augmentation.py
+++ data_augmentation.py
@@ -66,11 +66,21 @@ augmentation_pipeline = A.Compose(
         A.VerticalFlip(p=0.5),
         A.RandomRotate90(p=0.5),
         A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=30, p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
         OneOf(
             [
                 A.GaussNoise(var_limit=(10, 50), p=0.5), 
+                A.GaussianBlur(blur_limit=(3, 7), p=0.2),
             ],
             p=0.7,
+        ),
+        OneOf(
+            [
+                A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
+                A.RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, p=0.3),
+                A.ChannelShuffle(p=0.1),
+            ],
+            p=0.5,
         ),
     ]
 )
