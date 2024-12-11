99a1dc9b848aedfb558a2314b549b7ae7975e091
Levin <st166200@stud.uni-stuttgart.de>
2024-12-11 13:11:32 +0100

    changed saving in segformer

--- levin/models/segformer/segformer_b3.py
+++ levin/models/segformer/segformer_b3.py
@@ -80,7 +80,7 @@ class SegFormer:
         return validation_loss / total_val_samples
 
 
-    def train(self, dataloader, validationloader, criterion, save_path, epochs=10, learning_rate=1e-4, save=False):
+    def train(self, dataloader, validationloader, criterion, save_path, epochs=10, learning_rate=1e-4):
         """
         Fine-tune the SegFormer model on a dataset.
         :param dataloader: DataLoader object providing image-mask pairs
@@ -122,10 +122,9 @@ class SegFormer:
             avg_validation_loss = self.validate(validationloader, criterion)
             self.validation_losses[epoch + 1] = avg_validation_loss
 
-            if save:
-                epoch_save_path = save_path.replace(".pt", f"_epoch{epoch + 1}.pt")
-                torch.save(self.model.state_dict(), epoch_save_path)
-                print(f"Model saved to {epoch_save_path}")
+            epoch_save_path = save_path.replace(".pt", f"_epoch{epoch + 1}.pt")
+            torch.save(self.model.state_dict(), epoch_save_path)
+            print(f"Model saved to {epoch_save_path}")
 
             print(f"Epoch {epoch+1}/{epochs}, Training Loss: {avg_loss:.4f}, Validation Loss: {avg_validation_loss:.4f}")
 
